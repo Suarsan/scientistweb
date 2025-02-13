d59911c3ae7f37587249b42455efb63a045b1df7
Javier Suárez <javiersuarsan@gmail.com>
2025-02-13 17:45:22 -0500

    [feature] add footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'footer[appFooter]',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
   constructor(private translateService: TranslateService) { }
-
-  ngOnInit() {
-  }
 
   public spanish() {
     this.translateService.use('es');
   }
+
   public english() {
     this.translateService.use('en');
   }
+
   public languageInUse(lang) {
     if (this.translateService.currentLang === lang) {
       return true;
     }
     return false;
   }
+
 }
